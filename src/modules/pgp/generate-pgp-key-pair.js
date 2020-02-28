69ccd560355839e62f4347d2af85e0869e5e3145
Truong Chau Hien <truongchauhien@gmail.com>
2020-02-29 00:52:36 +0700

    Add PGP key pair generator.

---
+++ src/modules/pgp/generate-pgp-key-pair.js
@@ -0,0 +1,17 @@
+import openpgp from 'openpgp';
+import { promises as fs } from 'fs';
+
+const { writeFile } = fs;
+
+(async () => {
+    const { publicKeyArmored, privateKeyArmored, revocationCertificate } = await openpgp.generateKey({
+        userIds: [{ name: 'Truong Chau Hien', email: 'truongchauhien@gmail.com' }],
+        rsaBits: 4096
+    });
+
+    await Promise.all([
+        writeFile('pgp-public.key', publicKeyArmored),
+        writeFile('pgp-private.key', privateKeyArmored),
+        writeFile('pgp-revocation.key', revocationCertificate)
+    ]);
+})();
