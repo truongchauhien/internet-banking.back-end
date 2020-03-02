1fd9f0a004f80cfabff21111a44cab42c95b0703
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-02 17:50:57 +0700

    Add mysql database pool.

---
+++ src/database/mysql-db.js
@@ -0,0 +1,29 @@
+import mysql from 'mysql';
+import { promisify } from 'util';
+import config from '../configs/config-schema.js';
+
+export const pool = mysql.createPool({
+    connectionLimit: 255,
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    user: config.get('database.user'),
+    password: config.get('database.password'),
+    database: config.get('database.name')
+});
+
+/**
+ * 
+ * @param {string | mysql.Query} sql 
+ * @param {*} values 
+ */
+export const query = (sql, values) => {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, values, (error, results, fields) => {
+            if (error) {
+                resolve([results, fields]);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
