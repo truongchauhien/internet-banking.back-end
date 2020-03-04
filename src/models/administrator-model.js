cd3c2c30fd5e01cca769b70784dafd8b82b25638
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-04 17:13:23 +0700

    Add administrator model.

---
+++ src/models/administrator-model.js
@@ -0,0 +1,33 @@
+import { query } from '../database/mysql-db.js';
+
+export const getByUserName = async userName => {
+    const [results, fields] = await query('SELECT * FROM administrators WHERE userName = ?', [userName]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+};
+
+export const getByEmail = async email => {
+    const [results, fields] = await query('SELECT * FROM administrators WHERE email = ?', [email]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+};
+
+export const getById = async id => {
+    const [results] = await query('SELECT * FROM administrators WHERE id = ?', [id]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+}
+
+export const updateRefreshToken = (id, refreshToken) => {
+    const [results, fields] = query('UPDATE administrators SET refreshToken = ? WHERE id = ?', [refreshToken, id]);
+    return results.affectedRows;
+};
