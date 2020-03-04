fbdea7d017296f8539a28be99c28f1a8901b4735
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-04 17:11:01 +0700

    Add nodemailer transporter.

---
+++ src/modules/mail/mailer.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import config from '../../configs/config-schema.js';
+import logger from '../logger/logger.js';
+
+export const transporter = nodemailer.createTransport({
+    host: config.get('smtp.host'),
+    port: config.get('smtp.port'),
+    secure: config.get('smtp.secure'),
+    auth: {
+        user: config.get('smtp.auth.user'),
+        pass: config.get('smtp.auth.pass')
+    }
+});
+
+transporter.verify((err, success) => {
+    if (err) {
+        logger.error(err);
+    } else {
+        logger.info('SMTP server is ready to take our messages.');
+    }
+})
+
+export default transporter;
