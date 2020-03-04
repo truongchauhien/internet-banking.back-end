bb8d206e00d1785f7955157d81725951ccaa653b
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-04 17:13:03 +0700

    Add customer model.

---
+++ src/models/customer-model.js
@@ -0,0 +1,33 @@
+import { query } from '../database/mysql-db.js';
+
+export const getByUserName = async userName => {
+    const [results, fields] = await query('SELECT * FROM customers WHERE userName = ?', [userName]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+}
+
+export const getByEmail = async email => {
+    const [results, fields] = await query('SELECT * FROM customers WHERE email = ?', [email]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+};
+
+export const getById = async id => {
+    const [results] = await query('SELECT * FROM customers WHERE id = ?', [id]);
+    if (results) {
+        return results[0];
+    }
+
+    return null;
+}
+
+export const updateRefreshToken = async (id, refreshToken) => {
+    const [results, fields] = await query('UPDATE customers SET refreshToken = ? WHERE id = ?', [refreshToken, id]);
+    return results.affectedRows;
+};
