8a66fe02560098dd6944cf91093f91381b477853
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-06 03:40:13 +0700

    Add transaction support.

--- src/database/mysql-db.js
+++ src/database/mysql-db.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql';
-import { promisify } from 'util';
 import config from '../configs/config-schema.js';
+import logger from '../modules/logger/logger.js';
 
 export const pool = mysql.createPool({
     connectionLimit: 255,
@@ -16,7 +16,7 @@ export const pool = mysql.createPool({
  * @param {string | mysql.Query} sql 
  * @param {*} values 
  */
-export const query = (sql, values) => {
+export const pool_query = (sql, values) => {
     return new Promise((resolve, reject) => {
         pool.query(sql, values, (error, results, fields) => {
             if (error) {
@@ -27,3 +27,97 @@ export const query = (sql, values) => {
         });
     });
 };
+
+// this: mysql.Pool
+const _pool_getConnection = function () {
+    return new Promise((resolve, reject) => {
+        this.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(connection);
+            }
+        });
+    })
+};
+
+// this: mysql.Connection
+const _connection_release = function () {
+    this.release();
+};
+
+// this: mysql.Connection
+const _connection_beginTransaction = function () {
+    return new Promise((resolve, reject) => {
+        this.beginTransaction(err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+// this: mysql.Connection
+const _connection_commit = function () {
+    return new Promise((resolve, reject) => {
+        this.commit(err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+// this: mysql.Connection
+const _connection_rollback = function () {
+    return new Promise((resolve, reject) => {
+        this.rollback(err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+// this: mysql.Connection
+const _connection_query = function (sql, values) {
+    return new Promise((resolve, reject) => {
+        this.query(sql, values, (err, results, fields) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve([results, fields]);
+            }
+        });
+    });
+};
+
+export const getTransaction = async () => {
+    const pool_getConnection = _pool_getConnection.bind(pool);
+    const connection = await pool_getConnection();
+    return {
+        connection: connection,
+        release: _connection_release.bind(connection),
+        beginTransaction: _connection_beginTransaction.bind(connection),
+        commit: _connection_commit.bind(connection),
+        rollback: _connection_rollback.bind(connection),
+        query: _connection_query.bind(connection)
+    };
+};
+
+(async() => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            logger.err(err);
+        } else {
+            logger.info('The connection to MySQL server is OK!');
+            connection.release();
+        }
+    })
+})();
