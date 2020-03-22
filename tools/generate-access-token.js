03c0d08916bafb82a6210884432d7a6e0ea2962c
truongchauhien <truongchauhien@gmail.com>
2020-03-22 16:43:13 +0700

    Add access token generator tool.

---
+++ tools/generate-access-token.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import config from '../src/configs/config-schema.js';
+
+const TOKEN_SECRET_KEY = config.get('tokenSecretKey');
+
+const generateAccessToken = (payload) => {
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, TOKEN_SECRET_KEY, { expiresIn: '1d' }, (err, encoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(encoded);
+            }
+        })
+    });
+};
+
+generateAccessToken({ userType: 'customer', userId: 1 }).then(token => {
+    console.log(token);
+}, err => {
+    console.log(err);
+});
