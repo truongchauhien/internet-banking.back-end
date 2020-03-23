943d45c0031c124223e352057b4244fa81bc9c29
truongchauhien <truongchauhien@gmail.com>
2020-03-24 01:12:04 +0700

    Add account controller.

---
+++ src/controllers/customer-controllers/account-controller.js
@@ -0,0 +1,31 @@
+import * as accountModel from '../../models/account-model.js';
+import * as transactionModel from '../../models/transaction-model.js';
+import { HttpErrorClasses } from '../extensions/http-error.js';
+
+export const getAccounts = async (req, res) => {
+    const { userId: customerId } = req.auth;
+    const accounts = await accountModel.getAllAccounts(customerId);
+
+    return res.status(200).json({
+        accounts: accounts
+    });
+};
+
+export const getAccountTransactions = async (req, res) => {
+    const { userId: customerId } = req.auth;
+    const { accountNumber } = req.params;
+
+    const account = await accountModel.getAccount(accountNumber);
+
+    if (!account)
+        throw new HttpErrorClasses.NotFound();
+    if (customerId !== account.customerId)
+        throw new HttpErrorClasses.Forbidden();
+
+    const transactions = await transactionModel.findTransactionByAccountNumber(accountNumber);
+
+    return res.status(200).json({
+        accountNumber: accountNumber,
+        transactions: transactions
+    });
+};
