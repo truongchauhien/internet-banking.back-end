38cf878bb3a9a00dd38e24034be9ce72aeec2989
truongchauhien <truongchauhien@gmail.com>
2020-03-24 01:11:47 +0700

    Add transaction model.

---
+++ src/models/transaction-model.js
@@ -0,0 +1,24 @@
+import { doQuery } from '../database/mysql-db.js';
+
+export const findTransactionByAccountNumber = (accountNumber, from, to) => {
+    return doQuery(async (connection) => {
+        let results;
+
+        [results] = await connection.query('SELECT * FROM accounts WHERE accountNumber = ?', [accountNumber]);
+        if (!results) {
+            return [];
+        }
+
+        const account = results[0];
+
+        [results] = await connection.query(
+            'SELECT ts.id, ts.amount, tst.type, ts.createAt ' +
+            'FROM transactions ts ' +
+            'INNER JOIN transactions_types tst ON ts.typeId = tst.id' +
+            'WHERE accountId = ? AND WHERE ts.createAt BETWEEN ? AND ?',
+            [account.id, from, to]
+        );
+
+        return results;
+    });
+};
