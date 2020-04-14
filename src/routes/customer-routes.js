916a062256e045651a72298523be9c64e84c64ba
truongchauhien <truongchauhien@gmail.com>
2020-04-14 08:02:33 +0700

    Add transfer route to customer routes.

--- src/routes/customer-routes.js
+++ src/routes/customer-routes.js
@@ -3,10 +3,12 @@ import asyncWrapper from '../middlewares/async-wrapper.js';
 import verifyAccessToken from '../middlewares/verify-access-token.js';
 import accountRouter from './customer-routes/account-routes.js';
 import contactRouter from './customer-routes/contact-routes.js';
+import transferRouter from './customer-routes/transfer-routes.js';
 
 const router = express.Router();
 router.use(asyncWrapper(verifyAccessToken));
 router.use('/accounts', accountRouter);
 router.use('/contacts', contactRouter);
+router.use('/transfer', transferRouter);
 
 export default router;
