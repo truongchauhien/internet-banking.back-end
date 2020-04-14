2329af44c08a8dd23b27ab519750fcf160e3472d
truongchauhien <truongchauhien@gmail.com>
2020-04-14 07:56:46 +0700

    Refactoring error handling middleware.

--- src/middlewares/error-handling.js
+++ src/middlewares/error-handling.js
@@ -1,6 +1,8 @@
 import { HttpError, HttpErrorClasses } from '../controllers/extensions/http-error.js';
 import { MySqlError } from '../database/mysql-error.js';
+import { ERRORS } from '../controllers/extensions/errors.js';
 import logger from '../modules/logger/logger.js';
+import TransactionCanceled from '../models/extensions/transaction-canceled.js';
 
 /**
  * Error Handler Middleware.  
@@ -14,12 +16,38 @@ export const errorHandler = (err, req, res, next) => {
     }
 
     if (err instanceof HttpError) {
-        res.status(err.code).end();
+        res.status(err.code);
+        if (err.meta === undefined || err.meta === null) {
+            return res.end();
+        }
+        if (typeof err.meta === 'string') {
+            return res.json({
+                message: err.meta
+            });
+        } else {
+            return res.json(err.meta);
+        }
     } else if (err instanceof MySqlError) {
-        logger.error(err);
-        return res.status(500).end();
+        logger.error(JSON.stringify(err));
+        switch (err.code) {
+            case 'ER_DUP_ENTRY':
+                return res.status(400).json(ERRORS(1001));
+            default:
+                return res.status(500).end();
+        }
+    } else if (err instanceof TransactionCanceled) {
+        logger.error(JSON.stringify(err));
+        if (err.meta === undefined || err.meta === null) {
+            return res.status(400).end();
+        } else if (typeof err.meta === 'string') {
+            return res.status(400).json({
+                message: err.meta
+            });
+        } else {
+            return res.status(400).json(err.meta);
+        }
     } else {
-        logger.error(err);
+        logger.error(err.name + '\n' + err.message + '\n' + err.stack);
         return res.status(500).end();
     }
 };
