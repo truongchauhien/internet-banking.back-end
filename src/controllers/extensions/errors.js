dc6b10378e49d298fb2290f89b30af3d6d62c02a
truongchauhien <truongchauhien@gmail.com>
2020-04-14 07:42:09 +0700

    Add error constants.

---
+++ src/controllers/extensions/errors.js
@@ -0,0 +1,12 @@
+export const ERRORS = {
+    1001: {
+        code: 1001,
+        message: 'Duplicated entry.'
+    },
+    INCORRECT_OTP: {
+        code: 'INCORRECT_OTP',
+        message: 'OTP is incorrect.'
+    }
+};
+
+export default ERRORS;
