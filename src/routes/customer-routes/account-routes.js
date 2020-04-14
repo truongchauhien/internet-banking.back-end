43c9e14a4ecde304b2a0b5b6654193a625a4233b
truongchauhien <truongchauhien@gmail.com>
2020-04-14 08:01:42 +0700

    Add 'get account by account number' route.

--- src/routes/customer-routes/account-routes.js
+++ src/routes/customer-routes/account-routes.js
@@ -3,7 +3,10 @@ import asyncWrapper from '../../middlewares/async-wrapper.js';
 import * as accountController from '../../controllers/customer-controllers/account-controller.js';
 
 const router = express.Router();
+
+// Internal system accounts only.
 router.get('/', asyncWrapper(accountController.getAccounts));
+router.get('/:accountNumber', asyncWrapper(accountController.getAccountByAccountNumber));
 router.get('/:accountNumber/transactions', asyncWrapper(accountController.getAccountTransactions));
 
 export default router;
