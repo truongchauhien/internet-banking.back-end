d8ae56792d6ca0453b9b8d59eaed84610055efcb
truongchauhien <truongchauhien@gmail.com>
2020-04-19 01:30:20 +0700

    Add websocket support.

---
+++ src/web-socket/web-socket.js
@@ -0,0 +1,23 @@
+import url from 'url';
+import customerWss, { setup as setupCustomerWss } from './customer-websocket-server.js';
+
+export const integrate = (httpServer) => {
+    httpServer.on('upgrade', (request, socket, head) => {
+        const pathname = url.parse(request.url).pathname;
+
+        if (pathname === '/customer') {
+            customerWss.handleUpgrade(request, socket, head, ws => {
+                customerWss.emit('connection', ws, request);
+            });
+        } else {
+            socket.destroy();
+        }
+    });
+};
+
+let isSetup = false;
+export const setup = async () => {
+    if (isSetup) return;
+    await setupCustomerWss();
+    isSetup = true;
+};
