c04098983b390c88c899d35684fbe9f6001ebb89
truongchauhien <truongchauhien@gmail.com>
2020-04-19 01:39:51 +0700

    Refactoring transfer routes.

--- src/routes/customer-routes/transfer-routes.js
+++ src/routes/customer-routes/transfer-routes.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import asyncWrapper from '../../middlewares/async-wrapper.js';
-import {
-    createIntrabankTransfer,
-    getIntrabankTransfer,
-    confirmIntrabankTransfer,
-} from '../../controllers/customer-controllers/transfer-controller.js'
+import * as transferController from '../../controllers/customer-controllers/transfer-controller.js'
 
 const router = express.Router();
 
-router.post('/intrabank', asyncWrapper(createIntrabankTransfer));
-router.get('/intrabank/:transferId', asyncWrapper(getIntrabankTransfer));
-router.patch('/intrabank/:transferId', asyncWrapper(confirmIntrabankTransfer));
+router.post('/', transferController.createTransfer);
+router.get('/:transferId', transferController.getTransfer);
+router.post('/:transferId', transferController.confirmTransfer);
 
 export default router;
