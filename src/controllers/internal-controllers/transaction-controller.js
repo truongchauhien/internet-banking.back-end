60b72969da66f5ab810312591c75ece0435d34e0
truongchauhien <truongchauhien@gmail.com>
2020-04-29 16:18:44 +0700

    Refactoring transaction controller.

--- src/controllers/internal-controllers/transaction-controller.js
+++ src/controllers/internal-controllers/transaction-controller.js
@@ -1,22 +1,54 @@
 import * as transactionModel from '../../models/transaction-model.js';
+import * as accountModel from '../../models/account-model.js';
 import HttpErrorClasses from '../extensions/http-errors.js';
 
-export const getTransactions = async (req, res) => {
+export const getOwnTransactions = async (req, res) => {
     const { userId: customerId } = req.auth;
-    const { accountNumber, fromTime, toTime, pageNumber: rawPageNumber } = req.query;
+    const { accountId, startingAfter: rawStartingAfter } = req.query;
 
-    let pageNumber = Number.parseInt(rawPageNumber) || 1;
-    if (pageNumber <= 0) throw new HttpErrorClasses
-    const account = await accountModel.getAccount(accountNumber);
+    const startingAfter = Number.parseInt(rawStartingAfter) || null;
 
+    const account = await accountModel.getById(accountId);
     if (!account)
         throw new HttpErrorClasses.NotFound();
     if (customerId !== account.customerId)
         throw new HttpErrorClasses.Forbidden();
 
-    const transactions = await transactionModel.findByAccountNumber(accountNumber, fromTime, toTime, 10, pageNumber);
+    const limit = 1;
+    const transactions = await transactionModel.findByAccountId(account.id, limit + 1, startingAfter);
+
+    let hasMore = false;
+    if (Array.isArray(transactions) && transactions.length > limit) {
+        transactions.pop();
+        hasMore = true;
+    }
 
     return res.status(200).json({
-        transactions: transactions
+        transactions: transactions,
+        hasMore
+    });
+};
+
+export const getTransactions = async (req, res) => {
+    const { accountId, startingAfter: rawStartingAfter } = req.query;
+
+    const startingAfter = Number.parseInt(rawStartingAfter) || null;
+
+    const account = await accountModel.getById(accountId);
+    if (!account)
+        throw new HttpErrorClasses.NotFound();
+
+    const limit = 10;
+    const transactions = await transactionModel.findByAccountId(account.id, limit + 1, startingAfter);
+
+    let hasMore = false;
+    if (Array.isArray(transactions) && transactions.length > limit) {
+        transactions.pop();
+        hasMore = true;
+    }
+
+    return res.status(200).json({
+        transactions: transactions,
+        hasMore
     });
 };
