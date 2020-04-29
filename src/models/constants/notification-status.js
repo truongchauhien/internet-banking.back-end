e6ba82867018ad794a6ea183f0db558752c45f9d
truongchauhien <truongchauhien@gmail.com>
2020-04-29 16:05:13 +0700

    Add REVERSE_NOTIFICATION_STATUS.

--- src/models/constants/notification-status.js
+++ src/models/constants/notification-status.js
@@ -4,4 +4,10 @@ export const NOTIFICATION_STATUS = {
     HIDDEN: 3
 };
 
+export const REVERSE_NOTIFICATION_STATUS = {
+    1: 'UNREAD',
+    2: 'READ',
+    3: 'HIDDEN'
+};
+
 export default NOTIFICATION_STATUS;
