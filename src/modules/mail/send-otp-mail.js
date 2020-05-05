5cf8a844b793c423745ae96d97de1203448ccf44
truongchauhien <truongchauhien@gmail.com>
2020-05-05 18:27:35 +0700

    Refactoring otp mailer.

--- src/modules/mail/send-otp-mail.js
+++ src/modules/mail/send-otp-mail.js
@@ -1,19 +1,36 @@
 import mailer from './mailer.js';
 import config from '../../configs/configs.js';
+import logger from '../logger/logger.js';
 
 const OTP_SENDER = config.get('emailSenders.otp');
 
-export const sendOtpMail = ({ toEmail, customerName, otp }) => {
-    return mailer.sendMail({
+const sendOtpMail = ({ toEmail, subject, html }) => {
+    mailer.sendMail({
         from: OTP_SENDER,
         to: toEmail,
-        subject: 'Transfer Comfirmation-OTP',
-        html: generateEmailContent({ customerName, otp })
+        subject: subject,
+        html: html
+    }).catch(err => {
+        logger.error(err.name + '\n' + err.message + '\n' + err.stack);
     });
 };
 
-function generateEmailContent({ customerName, otp }) {
-    return `<p>Dear <b>${customerName},</b></p>
+export const sendOtpMailForTransferCofirmation = ({ toEmail, customerName, otp }) => {
+    sendOtpMail({
+        toEmail,
+        subject: 'Transfer Comfirmation',
+        html: `<p>Dear <b>${customerName},</b></p>
 <p>Here is verification code: <strong>${otp}</strong></p>
-<p>It expires in 5 minutes.</p>`;
-}
+<p>It expires in 5 minutes.</p>`
+    });
+};
+
+export const sendOtpMailForPasswordResetConfirmationMail = ({ toEmail, customerName, otp }) => {
+    sendOtpMail({
+        toEmail,
+        subject: 'Password Reset Confirmation',
+        html: `<p>Dear <b>${customerName},</b></p>
+        <p>Here is verification code: <strong>${otp}</strong></p>
+        <p>It expires in 5 minutes.</p>`
+    });
+};
