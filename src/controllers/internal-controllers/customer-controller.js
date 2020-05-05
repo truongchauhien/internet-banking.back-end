2748d6f9b71c7d9d9c7068308c8b15c436c14274
truongchauhien <truongchauhien@gmail.com>
2020-05-05 18:22:48 +0700

    Refactoring customer controller.

--- src/controllers/internal-controllers/customer-controller.js
+++ src/controllers/internal-controllers/customer-controller.js
@@ -1,15 +1,19 @@
 import bcrypt from 'bcrypt';
-import * as customerModel from '../../models/customer-model.js';
+import _ from 'lodash';
 import HttpErrors from '../extensions/http-errors.js';
+import generateSecret from '../../modules/otp/generate-secret.js';
+import * as customerModel from '../../models/customer-model.js';
 
 export const createCustomer = async (req, res) => {
-    const customer = _.pick(req.body, ['userName', 'password', 'email', 'fullName']);
-    const currentAccountNumber = await customerModel.createCustomer(customer);
-    // currentAccountNumber: Số tài khoản vãng lai.
+    const customer = _.pick(req.body, ['userName', 'password', 'fullName', 'email', 'phone']);
+    customer.password = await bcrypt.hash(customer.password, 12);
+    customer.otpSecret = await generateSecret();
+    const createdCustomer = await customerModel.createCustomer(customer);
+    delete createdCustomer['password'];
+    delete createdCustomer['otpSecret'];
     return res.status(201).json({
-        ...customer,
-        currentAccountNumber
-    })
+        customer: createdCustomer
+    });
 };
 
 export const createPasswordChangeForCustomer = async (req, res) => {
