1689a7fe7624a0fc087c9de7bdf1147882e23b1d
truongchauhien <truongchauhien@gmail.com>
2020-05-05 18:16:28 +0700

    Add findByCustomerId method for transaction model.

--- src/models/transaction-model.js
+++ src/models/transaction-model.js
@@ -17,3 +17,21 @@ export const findByAccountId = (accountId, limit, startingAfter) => {
         return results;
     });
 };
+
+export const findByCustomerId = (customerId, limit, startingAfter) => {
+    return doQuery(async (connection) => {
+        const [results] = await connection.query(
+            'SELECT ts.id, acc.accountNumber, ts.amount, ts.currencyId, tst.type, ts.createdAt ' +
+            'FROM transactions ts ' +
+            'INNER JOIN transaction_types tst ON ts.typeId = tst.id ' +
+            'INNER JOIN accounts acc ON ts.accountId = acc.id ' +
+            `WHERE ts.accountId IN (SELECT acc2.id FROM accounts acc2 WHERE acc2.customerId = ${mysql.escape(customerId)}) ` +
+            (startingAfter !== null ? `AND ts.id < ${mysql.escape(startingAfter)} ` : '') +
+            'ORDER BY ts.id DESC ' +
+            'LIMIT ?',
+            [limit]
+        );
+
+        return results;
+    });
+};
