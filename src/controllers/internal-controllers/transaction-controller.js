77cf565b4411e71b8629e9f5de59399a4107e5de
truongchauhien <truongchauhien@gmail.com>
2020-05-05 18:24:43 +0700

    Refactoring transaction controller.

--- src/controllers/internal-controllers/transaction-controller.js
+++ src/controllers/internal-controllers/transaction-controller.js
@@ -1,19 +1,20 @@
 import * as transactionModel from '../../models/transaction-model.js';
 import * as accountModel from '../../models/account-model.js';
+import * as customerModel from '../../models/customer-model.js';
 import HttpErrorClasses from '../extensions/http-errors.js';
 
 export const getOwnTransactions = async (req, res) => {
     const { userId: customerId } = req.auth;
     const { accountId, startingAfter: rawStartingAfter } = req.query;
 
-    const startingAfter = Number.parseInt(rawStartingAfter) || null;
-
+    if (!accountId) throw new HttpErrorClasses.BadRequest();
     const account = await accountModel.getById(accountId);
     if (!account)
         throw new HttpErrorClasses.NotFound();
     if (customerId !== account.customerId)
         throw new HttpErrorClasses.Forbidden();
 
+    const startingAfter = Number.parseInt(rawStartingAfter) || null;
     const limit = 1;
     const transactions = await transactionModel.findByAccountId(account.id, limit + 1, startingAfter);
 
@@ -30,16 +31,15 @@ export const getOwnTransactions = async (req, res) => {
 };
 
 export const getTransactions = async (req, res) => {
-    const { accountId, startingAfter: rawStartingAfter } = req.query;
+    const { userName, startingAfter: rawStartingAfter } = req.query;
+
+    if (!userName) throw new HttpErrorClasses.BadRequest();
+    const customer = await customerModel.getByUserName(userName);
+    if (!customer) throw new HttpErrorClasses.NotFound();
 
     const startingAfter = Number.parseInt(rawStartingAfter) || null;
-
-    const account = await accountModel.getById(accountId);
-    if (!account)
-        throw new HttpErrorClasses.NotFound();
-
-    const limit = 10;
-    const transactions = await transactionModel.findByAccountId(account.id, limit + 1, startingAfter);
+    const limit = 5;
+    const transactions = await transactionModel.findByCustomerId(customer.id, limit + 1, startingAfter);
 
     let hasMore = false;
     if (Array.isArray(transactions) && transactions.length > limit) {
