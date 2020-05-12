4c0e65429795b29e8024e7573d2d733852708437
truongchauhien <truongchauhien@gmail.com>
2020-05-12 15:42:35 +0700

    Refactoring account controller.

--- src/controllers/internal-controllers/account-controller.js
+++ src/controllers/internal-controllers/account-controller.js
@@ -1,5 +1,6 @@
 import * as accountModel from '../../models/account-model.js';
 import HttpErrors from '../extensions/http-errors.js';
+import bankingApiModules from '../../modules/banking-api-modules/banking-api-modules.js';
 
 export const getAccountsForCustomer = async (req, res) => {
     const { userId } = req.auth;
@@ -23,25 +24,57 @@ export const getAccountsForEmployee = async (req, res) => {
     });
 };
 
-export const getAccountForCustomer = async (req, res) => {
+export const getAccountForCustomer = (req, res) => {
+    const { bankId } = req.query;
+
+    if (bankId) {
+        return getInternalAccountForCustomer(req, res);
+    }
+
+    return getExternalAccountForCustomer(req, res);
+};
+
+async function getInternalAccountForCustomer(req, res) {
     const { userId: customerId } = req.auth;
-    const { identityValue } = req.params;
     const { identityType = 'id' } = req.query;
+    const { identity } = req.params;
 
     let account;
     switch (identityType) {
         case 'id':
-            account = await accountModel.getById(identityValue);
+            account = await accountModel.getById(identity);
             break;
         case 'accountNumber':
-            account = await accountModel.getByAccountNumber(identityValue);
+            account = await accountModel.getByAccountNumber(identity);
             break;
         default:
             throw new HttpErrors.BadRequest();
     }
 
     if (!account) throw new HttpErrors.NotFound();
-    if (customerId !== account.customerId) throw new HttpErrors.Forbidden();
+    if (customerId !== account.customerId) {
+        account = _.pick(account, ['id', 'accountNumber', 'fullName']);
+    }
+    account.holderName = account.fullName;
 
-    return res.status(200).json(account);
-};
+    return res.status(200).json({
+        account: account
+    });
+}
+
+async function getExternalAccountForCustomer(req, res) {
+    const { identity: accountNumber } = req.params;
+    const { bankId, identityType = 'accountNumber' } = req.query;
+
+    if (identityType !== 'accountNumber') throw new HttpErrors.BadRequest();
+
+    const bankingApiModule = bankingApiModules[bankId];
+    if (!bankingApiModule) throw new HttpErrors.BadRequest();
+
+    const account = await bankingApiModule.getAccount({ accountNumber: accountNumber });
+    if (!account) throw new HttpErrors.NotFound();
+
+    return res.status(200).json({
+        account: foundAccount
+    });
+}
