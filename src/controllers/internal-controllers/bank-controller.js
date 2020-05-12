854360e86e771e6aae4abb941a79b6d00fdf026b
truongchauhien <truongchauhien@gmail.com>
2020-05-12 15:42:01 +0700

    Add bank controller.

---
+++ src/controllers/internal-controllers/bank-controller.js
@@ -0,0 +1,8 @@
+import { getAllBanks } from "../../models/bank-model.js";
+
+export const getBanks = async (req, res) => {
+    const banks = await getAllBanks(['id', 'name', 'hasApi']);
+    return res.status(200).json({
+        banks
+    });
+};
