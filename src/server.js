75633e2d722da8c368a83938a3c11b051d18ec67
truongchauhien <truongchauhien@gmail.com>
2020-05-18 11:29:32 +0700

    Refactoring imports.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import http from 'http';
-import config from './modules/configs/configs.js';
+import configs from './modules/configs/configs.js';
 import logger from './modules/logger/logger.js';
 import restApi from './rest-api/index.js';
-import { integrate as integrateWebSocket, setup as setupWebSocket } from './websocket/integration.js';
-import { setup as setupBankingApiModules } from './modules/banking-api-modules/banking-api-modules.js';
 import { setup as setupRabbitMQ } from './modules/rabbitmq/rabbitmq.js';
-import { setup as setupCustomerNotificationService } from './modules/realtime-notifications/customer-notifications.js';
+import { setup as setupPushService } from './modules/push-service/index.js';
+import { integrateWebSocketToHttpServer, setup as setupWebSocket } from './websocket/index.js';
+import { setup as setupBankingApiModules } from './modules/banking-api-modules/banking-api-modules.js';
 
-const server = http.createServer();
-const PORT = config.get('port');
+const httpServer = http.createServer();
+const PORT = configs.get('port');
 (async () => {
     await setupRabbitMQ();
+    await setupPushService();
     await setupWebSocket();
-    await setupCustomerNotificationService();
     await setupBankingApiModules();
 
-    server.on('request', restApi);
-    integrateWebSocket(server);
-    server.listen(PORT, () => {
+    httpServer.on('request', restApi);
+    integrateWebSocketToHttpServer(httpServer);
+    httpServer.listen(PORT, () => {
         logger.info(`App listening on port ${PORT}.`);
     });
 })().catch(err => {
