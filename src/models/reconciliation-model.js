cf0b7da922578c28fe8e7a6966d72031d2616bc1
truongchauhien <truongchauhien@gmail.com>
2020-05-18 11:28:43 +0700

    Add new methods for reconciliation model.

--- src/models/reconciliation-model.js
+++ src/models/reconciliation-model.js
@@ -1,6 +1,31 @@
-import { pool_query } from '../modules/database/mysql-db.js';
+import { pool_query, doQuery } from '../modules/database/mysql-db.js';
+
+export const getAll = async () => {
+    const [results] = await pool_query('SELECT * FROM reconciliations ORDER BY id DESC');
+    return results;
+};
 
 export const create = async (reconciliation) => {
-    const [results] = await pool_query('INSERT INTO reconciliations SET ?', [reconciliation]);
-    return results.insertId;
+    return doQuery(async (connection) => {
+        let results;
+        [results] = await connection.query('INSERT INTO reconciliations SET ?', [reconciliation]);
+        const id = results.insertId;
+        [results] = await connection.query('SELECT * FROM reconciliations WHERE id = ?', [id]);
+        if (Array.isArray(results) && results.length > 0) {
+            return results[0];
+        }
+        return null;
+    });
+};
+
+export const update = (id, updates) => {
+    return pool_query('UPDATE reconciliations SET ? WHERE id = ?', [updates, id]);
+};
+
+export const deleteById = async (id) => {
+    const [results] = await pool_query('DELETE FROM reconciliations WHERE id = ?', [id]);
+    if (results.affectedRows > 0) {
+        return true;
+    }
+    return false;
 };
