b3ea307d9645e36ed8d93156d5298af1dd19eacd
truongchauhien <truongchauhien@gmail.com>
2020-05-20 13:49:39 +0700

    Keep logger silent while testing.

--- src/modules/logger/logger.js
+++ src/modules/logger/logger.js
@@ -1,4 +1,5 @@
 import winston from 'winston';
+import configs from '../configs/configs.js';
 
 export const logger = winston.createLogger({
     level: 'info',
@@ -6,10 +7,15 @@ export const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
             level: 'info',
+            silent: configs.get('env') === 'testing' || configs.get('env') === 'production',
             format: winston.format.combine(
                 winston.format.colorize(),
                 winston.format.simple()
             )
+        }),
+        new winston.transports.File({
+            filename: 'combined.log',
+            silent: configs.get('env') === 'testing' || configs.get('env') === 'development'
         })
     ]
 });
