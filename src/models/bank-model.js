01c9d14baf0a18de3053448659acf327d94b77d5
truongchauhien <truongchauhien@gmail.com>
2020-05-20 13:44:38 +0700

    Add getByPartnerCode for bank model.

--- src/models/bank-model.js
+++ src/models/bank-model.js
@@ -1,6 +1,15 @@
 import { pool_query } from '../modules/database/mysql-db.js';
 
-export const getAllBanks = async (fields = ['id', 'name', 'hasApi', 'partnerCode']) => {
+export const getAll = async (fields = ['id', 'name', 'hasApi', 'partnerCode']) => {
     const [results] = await pool_query('SELECT ?? FROM banks', [fields]);
     return results;
+};
+
+export const getByPartnerCode = async (partnerCode) => {
+    const [results] = await pool_query('SELECT * FROM banks WHERE partnerCode = ?', [partnerCode]);
+    if (Array.isArray(results) && results.length > 0) {
+        return results[0];
+    }
+
+    return null;
 };
