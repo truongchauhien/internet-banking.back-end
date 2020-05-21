3f2f0177525fdd054f09df347e6f818d2adf0272
truongchauhien <truongchauhien@gmail.com>
2020-05-21 14:24:00 +0700

    Add /charges endpoint for public api.

---
+++ src/rest-api/public/charges/charge-controller.js
@@ -0,0 +1,5 @@
+import HttpErrors from "../../commons/errors/http-errors.js";
+
+export const createCharge = (req, res) => {
+    throw new HttpErrors.Forbidden();
+};
--- src/rest-api/public/charges/charge-routes.js
+++ src/rest-api/public/charges/charge-routes.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import * as chargeController from './charge-controller.js';
 
 const router = express.Router();
+router.post('/', chargeController.createCharge);
 
 export default router;
